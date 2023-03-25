1198957b707c9ac922656becebdb0c6f2dab13c7
Mii <phongsak.ysd@gmail.com>
2023-03-25 17:03:43 +0700

    new validation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ProductController extends Controller
 {
@@ -15,43 +16,32 @@ class ProductController extends Controller
      */
     public function index()
     {
-        // $products = Product::where('id>0')->get();
         $products = Product::all();
-        // dd($products);
-        return view('product.index',compact(['products']));
+        return view('product.index', compact(['products']));
     }
 
-   
     public function create()
     {
         $categories = Category::all();
-        return view('product.create',compact(['categories']));
+        return view('product.create', compact(['categories']));
     }
 
-  
     public function store(Request $request)
     {
-        // dd($request->toArray());
-
         $validate = $request->validate([
             'name' => 'required|string|min:3',
             'price' => 'required|numeric|min:0|max:1000',
-            'category_id'=> 'required|exists:category,id',
+            'category_id' => 'required|exists:category,id',
             'slug' => 'required|string|min:3|unique:products,slug',
-
         ]);
-        // dd($validate);
+
         $product = new Product($validate);
         $product->save();
-        session()->flash('swal',"Create Success!");
+        session()->flash('swal', "Create Success!");
         return redirect()->route('product.index');
-        // $category = new Category($validate);
-        // $category->save();
-        // session()->flash('swal',"Create Success!");
-        //  return redirect()->route('product.index');
     }
 
-    
+
     public function show(Product $product)
     {
         //
@@ -65,11 +55,8 @@ class ProductController extends Controller
      */
     public function edit(Product $product)
     {
-
         $categories = Category::all();
-        return view('product.edit',compact(['product','categories']));
-
-
+        return view('product.edit', compact(['product', 'categories']));
     }
 
     /**
@@ -82,15 +69,17 @@ class ProductController extends Controller
     public function update(Request $request, Product $product)
     {
 
-       $validate = $request->validate([
+        $validate = $request->validate([
             'name' => 'required|string|min:3',
             'price' => 'required|numeric|min:0|max:1000',
-            'category_id'=> 'required|exists:category,id',
-            'slug' => 'required|string|min:3',
-
+            'category_id' => 'required|exists:category,id',
+            'slug' => [
+                'required',
+                'string|min:3',
+                // PS: เช็ค `slug` ซ้ำในตาราง `products` ข้ามการเช็คถ้าเป็น slug ของ product ตัวเดียวกัน
+                Rule::unique('product', 'slug')->ignore($product)
+            ],
         ]);
-
-
     }
 
     /**
